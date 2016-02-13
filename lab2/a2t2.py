51049ee6902046f77389759110b97dfbb1f8a216
andrewxu1993 <skycontainer@hotmail.com>
2016-02-13 14:19:19 -0500

    20160214

--- lab2/a2t2.py
+++ lab2/a2t2.py
@@ -4,103 +4,105 @@ import tensorflow as tf
 def accuracy(predictions, labels):
   return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))/ predictions.shape[0])
 
-with np.load("notMNIST.npz") as data:
-    images, labels = data["images"], data["labels"]
+def a2t2(batch_size,learning_rate):
+  with np.load("notMNIST.npz") as data:
+      images, labels = data["images"], data["labels"]
 
-image_size=28
-num_channels=1
-num_labels=10
-
-batch_size=50
-
-print ("Stamp 9")
-
-images=images.T.astype("float32")
-labels=np.eye(10)[labels[:,0]].astype("float32")
-
-s=images.shape
-images=images.reshape(s[0],image_size*image_size)
+  image_size=28
+  num_channels=1
+  num_labels=10
 
 
-train_dataset=images[0:15000]
-train_labels=labels[0:15000]
-valid_dataset=images[15000:16000]
-valid_labels=labels[15000:16000]
-test_dataset=images[16000:18720]
-test_labels=labels[16000:18720]
+  images=images.T.astype("float32")
+  labels=np.eye(10)[labels[:,0]].astype("float32")
 
-#print train_dataset.shape
-#print valid_dataset.shape
-#print test_dataset.shape
+  s=images.shape
+  images=images.reshape(s[0],image_size*image_size)
 
 
-hidden_num=1000
+  train_dataset=images[0:15000]
+  train_labels=labels[0:15000]
+  valid_dataset=images[15000:16000]
+  valid_labels=labels[15000:16000]
+  test_dataset=images[16000:18720]
+  test_labels=labels[16000:18720]
 
-graph = tf.Graph()
+  #print train_dataset.shape
+  #print valid_dataset.shape
+  #print test_dataset.shape
+
+
+  hidden_num=1000
+
+  graph = tf.Graph()
 
 
 
-with graph.as_default():
-  x_train=tf.placeholder(tf.float32,shape=(batch_size,image_size*image_size))
-  y_train=tf.placeholder(tf.float32,shape=(batch_size,num_labels))
+  with graph.as_default():
+    x_train=tf.placeholder(tf.float32,shape=(batch_size,image_size*image_size))
+    y_train=tf.placeholder(tf.float32,shape=(batch_size,num_labels))
 
 
-  # layer 1
-  w=tf.Variable(tf.truncated_normal([image_size*image_size,num_labels*hidden_num]))
-  b=tf.Variable(tf.truncated_normal([num_labels*hidden_num]))
+    # layer 1
+    w=tf.Variable(tf.truncated_normal([image_size*image_size,num_labels*hidden_num]))
+    b=tf.Variable(tf.truncated_normal([num_labels*hidden_num]))
 
-  # layer 2
-  w2=tf.Variable(tf.truncated_normal([num_labels*hidden_num,10]))
-  b2=tf.Variable(tf.truncated_normal([num_labels]))
-
-
-
-  #print x_train.get_shape()
-  logits=tf.matmul(x_train,w)
-  logits=tf.add(logits,b)
-  #print logits.get_shape()
-  #logits=tf.nn.relu(tf.reshape(logits,[batch_size,num_labels,hidden_num])+b)
-  logits=tf.nn.relu(logits)
-  #print logits.get_shape()
-  #print w2.get_shape()
-  #print b2.get_shape()
-  #print logits.get_shape()
-  logits=tf.matmul(logits,w2)
-  #print logits.get_shape()
-  logits=tf.add(logits,b2)
-  logits=tf.nn.relu(logits)
-  #print logits.get_shape()
-  #print y_train.get_shape()
-
-  cost=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits,y_train))
-
-  optimizer=tf.train.MomentumOptimizer(0.0001,0.0001).minimize(cost)
-
-  train_prediction=tf.nn.softmax(logits)
-  valid_prediction=tf.matmul(valid_dataset,w)+b
-  valid_prediction=tf.nn.softmax(tf.matmul(valid_prediction,w2)+b2)
-  test_prediction=tf.matmul(test_dataset,w)+b
-  test_prediction=tf.nn.softmax(tf.matmul(test_prediction,w2)+b2)
-
-step_num=1000
-
-with tf.Session(graph=graph) as session:
-  tf.initialize_all_variables().run()
-  print ("initialized")
-  va=[]
-  for step in range (step_num):
-    offset=(step*batch_size)%(train_labels.shape[0]-batch_size)
-    x_batch=train_dataset[offset:(offset+batch_size),:]
-    y_batch=train_labels[offset:(offset+batch_size),:]
+    # layer 2
+    w2=tf.Variable(tf.truncated_normal([num_labels*hidden_num,10]))
+    b2=tf.Variable(tf.truncated_normal([num_labels]))
 
 
-    feed_dict={x_train:x_batch,y_train:y_batch}
-    _,l,tp,vp=session.run([optimizer,cost,train_prediction,valid_prediction],feed_dict=feed_dict)
 
-    if (step%100==0):
-      print ("Minibatch loss at step %d: %f" %(step,l))
-      #print("Minibatch accuracy: %.1f%%" % accuracy(tp,y_batch))
-      va.append(accuracy(vp,valid_labels))
-      #print("Validation accuracy: %.1f%%" % accuracy(vp,valid_labels))
-  print (va)
-  print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(),test_labels))
+    #print x_train.get_shape()
+    logits=tf.matmul(x_train,w)
+    logits=tf.add(logits,b)
+    #print logits.get_shape()
+    #logits=tf.nn.relu(tf.reshape(logits,[batch_size,num_labels,hidden_num])+b)
+    logits=tf.nn.relu(logits)
+    #print logits.get_shape()
+    #print w2.get_shape()
+    #print b2.get_shape()
+    #print logits.get_shape()
+    logits=tf.matmul(logits,w2)
+    #print logits.get_shape()
+    logits=tf.add(logits,b2)
+    logits=tf.nn.relu(logits)
+    #print logits.get_shape()
+    #print y_train.get_shape()
+
+    cost=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits,y_train))
+
+    optimizer=tf.train.MomentumOptimizer(learning_rate,learning_rate).minimize(cost)
+
+    train_prediction=tf.nn.softmax(logits)
+    valid_prediction=tf.matmul(valid_dataset,w)+b
+    valid_prediction=tf.nn.softmax(tf.matmul(valid_prediction,w2)+b2)
+    test_prediction=tf.matmul(test_dataset,w)+b
+    test_prediction=tf.nn.softmax(tf.matmul(test_prediction,w2)+b2)
+
+  step_num=1000
+
+  with tf.Session(graph=graph) as session:
+    tf.initialize_all_variables().run()
+    print ("initialized")
+    va=[]
+    for step in range (step_num):
+      offset=(step*batch_size)%(train_labels.shape[0]-batch_size)
+      x_batch=train_dataset[offset:(offset+batch_size),:]
+      y_batch=train_labels[offset:(offset+batch_size),:]
+
+
+      feed_dict={x_train:x_batch,y_train:y_batch}
+      _,l,tp,vp=session.run([optimizer,cost,train_prediction,valid_prediction],feed_dict=feed_dict)
+
+      if (step%100==0):
+        print ("Minibatch loss at step %d: %f" %(step,l))
+        #print("Minibatch accuracy: %.1f%%" % accuracy(tp,y_batch))
+        va.append(accuracy(vp,valid_labels))
+        #print("Validation accuracy: %.1f%%" % accuracy(vp,valid_labels))
+    print (va)
+    print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(),test_labels))
+
+if __name__=="__main__":
+  print ("Stamp 1")
+  a2t2(50,0.001)
