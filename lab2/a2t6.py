c39b1e0f027f1467e4e3d5ae58032e0c641d92a1
andrewxu1993 <skycontainer@hotmail.com>
2016-02-13 22:50:53 -0500

    20160214

--- lab2/a2t6.py
+++ lab2/a2t6.py
@@ -343,7 +343,7 @@ if __name__=="__main__":
     learning_r.append(lr)
     layer_n.append(ln)
     hidden_n.append(hn)
-    ln=2
+    ln=3
     if ln==2:
       vas.append(l2(100,lr,hn)) # the best
     elif ln==3:
