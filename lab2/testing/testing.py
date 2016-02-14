62de6c92462c49747b40adb1c9a128378b7a579c
andrewxu1993 <skycontainer@hotmail.com>
2016-02-13 20:54:09 -0500

    20160214

--- lab2/testing/testing.py
+++
@@ -1,9 +0,0 @@
-from __future__ import print_function
-import numpy as np
-import tensorflow as tf
-from six.moves import range
-from six.moves import cPickle as pickle
-
-a=[1,2,3]
-
-print (a[-1])
